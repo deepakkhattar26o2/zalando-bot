0306fbd33942b166b6d54715b14df29139635ec2
deepakkhattar26o2 <74506752+deepakkhattar26o2@users.noreply.github.com>
2022-03-03 20:27:54 +0530

    a lil better

--- main.py
+++ main.py
@@ -5,34 +5,35 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 s = Service(r'E:\geckodriver.exe')
-driver = webdriver.Firefox(service=s)
 file = open('listo.csv')
 filename = 'res.csv'
 csvreader = csv.reader(file)
 header = []
 header = next(csvreader)
-def logout():
-    try : 
-        driver.get('https://en.zalando.de/logout/')
-    except:
-        print('uh oh!')
+# def driver.close():
+#     try : 
+#         driver.get('https://en.zalando.de/logout/')
+#     except:
+#         print('uh oh!')
 with open(filename, 'w', newline="") as file:
     csvwriter = csv.writer(file)
     csvwriter.writerow(['Email','Password','LOGIN_STATUS','CART_STATUS'])
     for row in csvreader :
+        driver = webdriver.Firefox(service=s)
         try:
+            driver.get('https://en.zalando.de/')
             driver.get('https://en.zalando.de/login/')
             element = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.ID, 'login.email'))
             )
-            driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+            # driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
             driver.find_element(By.ID, 'login.email').send_keys(row[0].strip())
             driver.find_element(By.ID, 'login.secret').send_keys(row[1].strip())
             driver.find_element(By.XPATH, '//button[@aria-label="Login"]').click()
         except : 
             print('DEVLOG Login Process Failed', row[0].strip())
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'Fail', 'Fail'])
-            logout()
+            driver.close()
             continue
         try:
             element = WebDriverWait(driver, 10).until(
@@ -41,7 +42,7 @@ with open(filename, 'w', newline="") as file:
             )
             print('Login Failed', row[0].strip())
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'FAIL', 'FAIL'])
-            logout()
+            driver.close()
             continue
         except:
             print('DEVLOG Logged In Successfully')
@@ -55,7 +56,7 @@ with open(filename, 'w', newline="") as file:
         except:
             print('DEVLOG Button Not Found Timed Out', row[0].strip())
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
+            driver.close()
             continue
         try:
             driver.find_element(By.XPATH, '//button[@id="uc-btn-accept-banner"]').click()
@@ -63,7 +64,7 @@ with open(filename, 'w', newline="") as file:
         except:
             print('DEVLOG Hover Button Click Didn\'t Work')
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
+            driver.close()
             continue
 
 
@@ -73,7 +74,7 @@ with open(filename, 'w', newline="") as file:
         except:
             print('DEVLOG Error in Opening Product Page')
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
+            driver.close()
             continue
 
         print('DEVLOG Product Page Reached!')
@@ -86,7 +87,7 @@ with open(filename, 'w', newline="") as file:
         except:
             print('DEVLOG Button Not Found Timed Out')
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
+            driver.close()
             continue
 
         try:
@@ -94,7 +95,7 @@ with open(filename, 'w', newline="") as file:
         except:
             print('DEVLOG List Clicker Didn\'t Work')
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
+            driver.close()
             continue
 
         print('DEVLOG List Opened')
@@ -107,7 +108,7 @@ with open(filename, 'w', newline="") as file:
         except:
             print('DEVLOG Label Not Found Timed Out')
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
+            driver.close()
             continue
 
         shoe_list = driver.find_elements(
@@ -122,7 +123,7 @@ with open(filename, 'w', newline="") as file:
         except:
             print('DEVLOG Span Not Found TImed Out')
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
+            driver.close()
             continue
         for i in shoe_list:
             try:
@@ -130,7 +131,6 @@ with open(filename, 'w', newline="") as file:
                 print('DEVLOG unavailable shoe')
             except:
                 i.click()
-                cond = False
                 break
 
         try:
@@ -139,7 +139,7 @@ with open(filename, 'w', newline="") as file:
         except  :
             print('DEVLOG Add to Bag Failed', row[0].strip())
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
+            driver.close()
             continue
 
         try:
@@ -148,7 +148,7 @@ with open(filename, 'w', newline="") as file:
         except:
             print('DEVLOG Error in Opening Cart Page', row[0].strip())
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
+            driver.close()
             continue
 
         print('DEVLOG Cart Page Reached!')
@@ -156,27 +156,18 @@ with open(filename, 'w', newline="") as file:
         try:
             element = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located(
-                    (By.XPATH, "//span[text()='Love it again']"))
+                    (By.XPATH, "//h4[@class='z-2-text z-coast-base__article__name z-2-text-body-small-regular z-2-text-gray' and  text()='WS327 - Trainers - dark violet/glow']"))
             )
-            print('Devlog Cart Text Found')
-        except:
-            print('DEVLOG Cart Not Found Timed Out', row[0].strip())
-            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
-            continue
-
-        try:
-            driver.find_element(By.XPATH, "//h4[@class='z-2-text z-coast-base__article__name z-2-text-body-small-regular z-2-text-gray' and  text()='WS327 - Trainers - dark violet/glow']")
             print('DEVLOG Shoe Found In Cart', row[0].strip())
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'PASS'])
-            logout()
+            driver.close()
             continue
         except:
             print('DEVLOG Shoe Not Found in Cart', row[0].strip())
             csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
-            logout()
+            driver.close()
             continue
 file.close()
-driver.quit()
+# driver.quit()
 #//h2[text()='Your bag']
 #//div[@class="z-coast-fjord-miniCart_articleProductName" and text()='KENT COLLAR LONG SLEEVE - Shirt - stormy sea']
