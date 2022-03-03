54a175901886ee0c7e84b7c11434dda0220084c6
deepakkhattar26o2 <74506752+deepakkhattar26o2@users.noreply.github.com>
2022-03-03 17:57:14 +0530

    Working Flow Before Proxy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from dotenv import load_dotenv
-import os
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,82 +6,177 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 s = Service(r'E:\geckodriver.exe')
 driver = webdriver.Firefox(service=s)
-load_dotenv()
-username = os.getenv('uname')
-pswd = os.getenv('pswd')
-driver.get('https://en.zalando.de/login/')
-element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, 'login.email'))
-)
-driver.find_element(By.ID, 'login.email').send_keys('61333c@libero.it')
-driver.find_element(By.ID, 'login.secret').send_keys('cb40903333')
-driver.find_element(By.XPATH, '//button[@aria-label="Login"]').click()
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, '//div[@data-testid="login_error_notification"]'))
-    )
-except:
-    print('Logged In Successfully')
-
-
-try:
-    driver.get(
-        'https://en.zalando.de/new-balance-ws327-trainers-dark-violetglow-ne211a0kd-t11.html?_rfl=de')
-except:
-    print('Error in Opening Site')
-
-print('DEVLOG Product Page Reached!')
-
-try:
-    element = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located(
-            (By.XPATH, '//button[@id="uc-btn-accept-banner"]'))
-    )
-except:
-    print('Button Not Found Timed Out')
-
-driver.find_element(By.XPATH, '//button[@id="uc-btn-accept-banner"]').click()
-
-
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, '//button[@id="picker-trigger"]'))
-    )
-except:
-    print('Button Not Found Timed Out')
-
-try:
-    driver.find_element(By.XPATH, '//button[@id="picker-trigger"]').click()
-except:
-    print('List Clicker Didn\'t Work')
-
-print('DEVLOG List Opened')
-
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, '//label[contains(@for, "size-picker-")]'))
-    )
-except:
-    print('Label Not Found Timed Out')
-
-shoe_list = driver.find_elements(
-    By.XPATH, '//label[contains(@for, "size-picker-")]')
-print('Length of List : ', len(shoe_list))
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//span[text()='Notify Me']"))
-    )
-except:
-    print('Span Not Found TImed Out')
-for i in shoe_list:
-    try:
-        i.find_element(By.XPATH, ".//span[text()='Notify Me']")
-        print('unavailable shoe')
+file = open('listo.csv')
+filename = 'res.csv'
+csvreader = csv.reader(file)
+header = []
+header = next(csvreader)
+def logout():
+    try : 
+        driver.get('https://en.zalando.de/logout/')
     except:
-        i.click()
-        break
-driver.find_element(By.XPATH, '//button[@aria-label="Add to bag"]').click()
+        print('uh oh!')
+with open(filename, 'w', newline="") as file:
+    csvwriter = csv.writer(file)
+    csvwriter.writerow(['Email','Password','LOGIN_STATUS','CART_STATUS'])
+    for row in csvreader :
+        try:
+            driver.get('https://en.zalando.de/login/')
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, 'login.email'))
+            )
+            driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+            driver.find_element(By.ID, 'login.email').send_keys(row[0].strip())
+            driver.find_element(By.ID, 'login.secret').send_keys(row[1].strip())
+            driver.find_element(By.XPATH, '//button[@aria-label="Login"]').click()
+        except : 
+            print('DEVLOG Login Process Failed', row[0].strip())
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'Fail', 'Fail'])
+            logout()
+            continue
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//div[@data-testid="login_error_notification"]'))
+            )
+            print('Login Failed', row[0].strip())
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'FAIL', 'FAIL'])
+            logout()
+            continue
+        except:
+            print('DEVLOG Logged In Successfully')
+
+        try:
+            element = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//button[@id="uc-btn-accept-banner"]'))
+            )
+            print('Hover Button Found ehehe')
+        except:
+            print('DEVLOG Button Not Found Timed Out', row[0].strip())
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+        try:
+            driver.find_element(By.XPATH, '//button[@id="uc-btn-accept-banner"]').click()
+            print('DEVLOG Hover Button Click Worked haha')
+        except:
+            print('DEVLOG Hover Button Click Didn\'t Work')
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+
+
+        try:
+            driver.get(
+                'https://en.zalando.de/new-balance-ws327-trainers-dark-violetglow-ne211a0kd-t11.html?_rfl=de')
+        except:
+            print('DEVLOG Error in Opening Product Page')
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+
+        print('DEVLOG Product Page Reached!')
+
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//button[@id="picker-trigger"]'))
+            )
+        except:
+            print('DEVLOG Button Not Found Timed Out')
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+
+        try:
+            driver.find_element(By.XPATH, '//button[@id="picker-trigger"]').click()
+        except:
+            print('DEVLOG List Clicker Didn\'t Work')
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+
+        print('DEVLOG List Opened')
+
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//label[contains(@for, "size-picker-")]'))
+            )
+        except:
+            print('DEVLOG Label Not Found Timed Out')
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+
+        shoe_list = driver.find_elements(
+            By.XPATH, '//label[contains(@for, "size-picker-")]')
+        print('DEVLOG Length of List : ', len(shoe_list))
+
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//span[text()='Notify Me']"))
+            )
+        except:
+            print('DEVLOG Span Not Found TImed Out')
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+        for i in shoe_list:
+            try:
+                i.find_element(By.XPATH, ".//span[text()='Notify Me']")
+                print('DEVLOG unavailable shoe')
+            except:
+                i.click()
+                cond = False
+                break
+
+        try:
+            driver.find_element(By.XPATH, '//button[@aria-label="Add to bag"]').click()
+            print('DEVLOG Add to Bag Clicked')
+        except  :
+            print('DEVLOG Add to Bag Failed', row[0].strip())
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+
+        try:
+            driver.get(
+                'https://en.zalando.de/cart/')
+        except:
+            print('DEVLOG Error in Opening Cart Page', row[0].strip())
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+
+        print('DEVLOG Cart Page Reached!')
+
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//span[text()='Love it again']"))
+            )
+            print('Devlog Cart Text Found')
+        except:
+            print('DEVLOG Cart Not Found Timed Out', row[0].strip())
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+
+        try:
+            driver.find_element(By.XPATH, "//h4[@class='z-2-text z-coast-base__article__name z-2-text-body-small-regular z-2-text-gray' and  text()='WS327 - Trainers - dark violet/glow']")
+            print('DEVLOG Shoe Found In Cart', row[0].strip())
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'PASS'])
+            logout()
+            continue
+        except:
+            print('DEVLOG Shoe Not Found in Cart', row[0].strip())
+            csvwriter.writerow([row[0].strip(), row[1].strip(), 'PASS', 'FAIL'])
+            logout()
+            continue
+file.close()
+driver.quit()
+#//h2[text()='Your bag']
+#//div[@class="z-coast-fjord-miniCart_articleProductName" and text()='KENT COLLAR LONG SLEEVE - Shirt - stormy sea']
